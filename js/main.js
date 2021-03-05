9b7d4fca3ead0e121017dee219bef2edf612a78e
Smith Thay <contact.smith.thay@gmail.com>
2021-03-05 10:00:07 -0500

    degree

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+
+//create all variable that grab ids from HTML
 const inputZip = document.getElementById("zipSearch");
 const button = document.getElementById("button");
 const city = document.getElementById('city');
@@ -6,28 +8,32 @@ const tempF = document.getElementById('tempF');
 const tempC = document.getElementById('tempC');
 const description = document.getElementById('description')
 const icons = document.getElementById('icons');
-let zip = "";
-let data ="";
+//add event listener for the getWeather function 
 button.addEventListener('click', getWeather);
-
+//function to get weather
 function getWeather(){
+    //zip is has been grabbed by zip
     let zip = document.getElementById('zipSearch').value;
+    //personal api key
     apiKey= "8115cfaacb8cef870e290650830cfc00";
+    //api url with zip and api key as variables to be inputted 
     apiUrl = `https://api.openweathermap.org/data/2.5/weather?zip=${zip},&appid=${apiKey}`
-  
+  //fetch api url
     fetch(apiUrl)
         .then(function(response){
+            //turns it into JSON
         return response.json();
         })
         .then(function(json) {
+            //turns it into data variable
             data = json
             city.innerHTML = data.name;
-            tempK.innerHTML = data.main.temp + ' K'
+            tempK.innerHTML = data.main.temp + '°' +' K'
             tempF.innerHTML = Math.round((data.main.temp - 273.15) * 9 / 5 + 32) + '°' + ' F'
             tempC.innerHTML = Math.round(json.main.temp - 273.15) + '°' + ' C'
             description.innerHTML = data.weather[0].description
             icons.src = 'https://openweathermap.org/img/wn/' + data.weather[0].icon + '@2x.png';
-        })
+        })//if it is not a valid zip
         .catch(function(error) {
             alert("Please Enter A Valid Zip!");
             city.innerHTML= "Please Enter A Valid Zip"
