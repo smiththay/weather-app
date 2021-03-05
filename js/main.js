ccc3099367724d20f6a808de8d679fb2fecdf025
Smith Thay <contact.smith.thay@gmail.com>
2021-03-04 19:57:54 -0500

    functioning error messag

--- js/main.js
+++ js/main.js
@@ -6,19 +6,15 @@ const tempF = document.getElementById('tempF');
 const tempC = document.getElementById('tempC');
 const description = document.getElementById('description')
 const icons = document.getElementById('icons');
-
 let zip = "";
 let data ="";
-
 button.addEventListener('click', getWeather);
 
 function getWeather(){
-
     let zip = document.getElementById('zipSearch').value;
     apiKey= "8115cfaacb8cef870e290650830cfc00";
     apiUrl = `https://api.openweathermap.org/data/2.5/weather?zip=${zip},&appid=${apiKey}`
   
-
     fetch(apiUrl)
         .then(function(response){
         return response.json();
@@ -26,15 +22,15 @@ function getWeather(){
         .then(function(json) {
             data = json
             city.innerHTML = data.name;
-            tempK.innerHTML = data.main.temp + " k"
-            tempF.innerHTML = Math.round((data.main.temp - 273.15) * 9 / 5 + 32) + ' F'
-            tempC.innerHTML = Math.round(json.main.temp - 273.15) + ' C'
+            tempK.innerHTML = data.main.temp + ' K'
+            tempF.innerHTML = Math.round((data.main.temp - 273.15) * 9 / 5 + 32) + '°' + ' F'
+            tempC.innerHTML = Math.round(json.main.temp - 273.15) + '°' + ' C'
             description.innerHTML = data.weather[0].description
             icons.src = 'https://openweathermap.org/img/wn/' + data.weather[0].icon + '@2x.png';
         })
-        .catch(function(error){
-            console.error('Error:', error);
-        
+        .catch(function(error) {
+            alert("Please Enter A Valid Zip!");
+            city.innerHTML= "Please Enter A Valid Zip"
         });
         
     }
